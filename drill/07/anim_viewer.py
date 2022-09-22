ae6120b40f6dee69494dbc9523b5c6cd58f4c79a
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-09-22 17:21:23 +0900

    조건 수정

--- drill/07/anim_viewer.py
+++ drill/07/anim_viewer.py
@@ -7,7 +7,7 @@ action = 1010
 frame_count = [15,11,11,11,10,11,11,14,14,11,15]
 index =0
 
-while action > 0 & frame < 15:
+while action > 0:
     clear_canvas()
     character.clip_draw(frame * 100, action, 100, 110, 400, 300)
     update_canvas()

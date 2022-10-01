04b51113c88bd16bc995a550a2a0347561f69858
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-10-02 01:00:48 +0900

    sprtite

--- TermProject/SuperMario/anim_viewer.py
+++ TermProject/SuperMario/anim_viewer.py
@@ -20,26 +20,41 @@ def handle_events():
         elif event.type ==SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir-= 1
-                character =load_image('idle.png')
+                character =load_image('idle_right.png')
                 action = 5
              
             if event.key == SDLK_LEFT:
                 dir+= 1
-                character =load_image('idle.png')
-                action = 70
+                character =load_image('idle_left.png')
+                action = 5
            
 running = True
 open_canvas(1280,1024)
 
 frame = 0
+bframe = 0
+block_frame = 0
 dir = 0
 x= 800//2
-action  = 5
-character =load_image('idle.png')
+action  = 3
+#character =load_image('jump.png')
+character =load_image('idle_right.png')
+icblock =load_image('block_1.png')
+bblock =load_image('block_2.png')
+mbos = load_image('jr_coopa.png')
+grass =load_image('grass.png')
 
 while running:
     clear_canvas()
+    # jump: character.clip_draw(frame * 50,  action , 50, 75, x, 110)
     character.clip_draw(frame * 50,  action , 50, 60, x, 90)
+    mbos.clip_draw(bframe * 50,  70 , 50, 60, 500, 90)
+    
+    grass.draw(400,35)
+    icblock.clip_draw(block_frame * 30,  0 , 30, 35, 400, 200)
+    bblock.clip_draw(block_frame * 30,  0 , 30, 35, 425, 200)
+    block_frame = (block_frame+1) % 4
+    delay(0.1)
     update_canvas()
     
     handle_events()
@@ -53,5 +68,6 @@ while running:
         frame = (frame + 1) % 79
         delay(0.03)
 
-   
+    bframe = (bframe + 1) % 19
+    
 close_canvas()

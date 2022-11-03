f91d86a6b50ce304e0e9b49f4f63aa4fdc3c3520
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-03 13:13:10 +0900

    sleep 높이 수정

--- drill/12/boy.py
+++ drill/12/boy.py
@@ -88,10 +88,10 @@ class SLEEP:
     def draw(self):
         if self.face_dir == -1:
             self.image.clip_composite_draw(self.frame * 100, 200, 100, 100,
-                                          -3.141592 / 2, '', self.x + 25, self.y - 25, 100, 100)
+                                          -3.141592 / 2, '', self.x + 25, self.y - 15, 100, 100)
         else:
             self.image.clip_composite_draw(self.frame * 100, 300, 100, 100,
-                                          3.141592 / 2, '', self.x - 25, self.y - 25, 100, 100)
+                                          3.141592 / 2, '', self.x - 25, self.y - 15, 100, 100)
 
 
 #3. 상태 변환 구현

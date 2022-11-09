14ec24423033116076678621fa2b8586679cdfa3
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-09 17:14:25 +0900

    Update birds.py

--- drill/13/birds.py
+++ drill/13/birds.py
@@ -7,6 +7,9 @@ RUN_SPEED_KMPH = 10.0 # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 4
 
 class BIRD:
     image = None
@@ -17,12 +20,21 @@ class BIRD:
         self.x_dir = 1
         self.y_dir = 1
         self.reflect = ' '
-        self.frame = 0
+        self.clip = 4
+        self.action = 0
+        self.frame = random.randint(0,3)
     def draw(self):
-        self.image.clip_composite_draw(int(self.frame) * 184, 169, 184, 169, 0, self.reflect, self.x, self.y, 50, 50)
+        self.image.clip_composite_draw(int(self.frame) * 184, self.action , 184, 169, 0, self.reflect, self.x, self.y, 50, 50)
 
     def update(self):
-        self.frame = (self.frame + 1) % 4
+        #self.frame = (self.frame + 1) % self.clip
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % self.clip
+        if self.frame == 0:
+            self.action += 169
+            self.clip = 5
+            if self.action > 169 * 2:
+                self.action = 0
+                self.clip = 4
         self.x += self.x_dir * RUN_SPEED_PPS * game_framework.frame_time
         self.y += self.y_dir * RUN_SPEED_PPS * game_framework.frame_time
 
